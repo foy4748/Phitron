6b441f76fec6d70307996dd9c417b17b515afb81
foy4748 <faisaljfcl@gmail.com>
2023-09-23 01:56:59 +0600

    Tried the Triangle Pattern

---
+++ Semester1/patterns.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+
+#define dimenstion 5
+
+//Print some patterns
+
+// Triangle |_
+void triangle(int n){
+	int i,j;
+	for(i=0; i<n; i++){
+		for(j=0; j<=i;j++){
+			printf("*\t");
+		}
+		printf("\n");
+	}
+}
+
+// Triangle |^
+void triangleB(int n){
+	int i,j;
+	for(i=n; i>=0; i--){
+		for(j=0; j<i;j++){
+			printf("*\t");
+		}
+		printf("\n");
+	}
+}
+
+void triangleC(int n){
+	int i,j;
+	for(i=n; i>=0; i--){
+		for(j=0; j<n; j++){
+			if(n - j > i){
+				printf("\t");
+			}
+			else{
+				printf("*\t");
+			}
+		}
+		printf("\n");
+	}
+}
+void triangleD(int n){
+	int i,j;
+	for (i = 0; i < n; i++) {
+		
+	}
+}
+
+// Driver Code
+int main(){
+	printf("Triangle |_ \n");
+	triangle(dimenstion);
+	printf("Triangle B |^ \n");
+	triangleB(dimenstion);
+	printf("Triangle C ^| \n");
+	triangleC(dimenstion);
+	printf("Triangle D ^| \n");
+	triangleD(dimenstion);
+	return 0;
+}
