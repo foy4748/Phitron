85d282e667cb534e128e66b634d8b9d47089692c
foy4748 <faisaljfcl@gmail.com>
2024-01-07 01:25:52 +0600

    Solved first two problems from Module 19.5

---
+++ Semester2/003_Introduction to Data Structure/Module_19.5_Solve_LeetCode_Problem/univalued_binary_tree.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// LeetCode
+// https://leetcode.com/problems/univalued-binary-tree
+class Solution {
+public:
+
+    bool isUnivalTree(TreeNode* root) {
+        if(!root) return true;
+        bool isLeftSame = root->left ? root->val == root->left->val : true;
+        bool isRightSame = root->right ? root->val == root->right->val : true;
+        bool isLeftAllSame = isUnivalTree(root->left) && isLeftSame;
+        bool isRightAllSame = isUnivalTree(root->right) && isRightSame;
+        return isLeftAllSame && isRightAllSame;
+
+
+    }
+};
