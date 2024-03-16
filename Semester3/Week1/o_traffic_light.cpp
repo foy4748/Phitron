589cbcd27848f48d203e82e79688e1adefbadbfe
foy4748 <faisaljfcl@gmail.com>
2024-03-16 14:27:20 +0600

    Succesfully Solved Week1 0 Traffic Light
    Thanks to this youtube video
    for giving a clear view of the problem
    
    https\://www.youtube.com/watch?v=WJ0-B-g82v0&t=436s

--- Semester3/Week1/o_traffic_light.cpp
+++ Semester3/Week1/o_traffic_light.cpp
@@ -1,3 +1,4 @@
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -8,38 +9,33 @@ int main() {
   cin >> T;
   while (T--) {
     int N;
-    char s;
+    char s; // starting char
     string sequence;
+
     cin >> N >> s >> sequence;
+
+    int sequence_size = sequence.size();
 
     if (s == 'g') {
       cout << 0 << "\n";
       continue;
     }
 
-    vector<int> g_idx;
-    for (int i = 0; i < N; i++) {
+    sequence += sequence;
 
-      int next_idx = (i + 1) % N;
-      if (sequence[i] == 'g' && sequence[next_idx] == s) {
-        g_idx.push_back(next_idx);
+    int result = 0, count = 0;
+    for (int i = 0; i < sequence_size; i++) {
+      if (sequence[i] == s) {
+        count = 0;
+        while (sequence[i] != 'g') {
+          count++;
+          i++;
+        }
+        result = max(result, count);
       }
     }
 
-    int gurenteed_time = 0;
-    for (int c : g_idx) {
-      int current_time = 0;
-      while (sequence[c] != 'g') {
-        current_time++;
-        c = (c + 1) % N;
-      }
-      if (current_time > gurenteed_time) {
-        gurenteed_time = current_time;
-      }
-    }
-    if (g_idx.size() == 0)
-      gurenteed_time = 1;
-    cout << gurenteed_time << "\n";
+    cout << result << "\n";
   }
   return 0;
 }
