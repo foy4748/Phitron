3c35149e8bdd88ac82e2b54c548445b157a17b5a
foy4748 <faisaljfcl@gmail.com>
2024-05-19 02:18:25 +0600

    CSES Playlist 1141

--- Semester3/Week9/a_playlist.cpp
+++ Semester3/Week9/a_playlist.cpp
@@ -9,7 +9,25 @@ int main() {
   cin.tie(NULL);
 
   // Write your code here
+  int N;
+  cin >> N;
+  vector<int> A(N);
+  set<int> st;
+  int ans = 0;
+  int startIdx = 0;
+  for (int i = 0; i < N; i++) {
+    cin >> A[i];
+  }
 
+  for (int i = 0; i < N; i++) {
+    while (st.count(A[i])) {
+      st.erase(A[startIdx]);
+      startIdx++;
+    }
+    st.insert(A[i]);
+    ans = max(ans, (int)st.size());
+  }
+  cout << ans << "\n";
   /*
   int T; cin>>T;
   while(T--){
