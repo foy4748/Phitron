40475fbe59823de5f553acf52250730c209d3ee8
foy4748 <faisaljfcl@gmail.com>
2024-06-07 00:17:47 +0600

    Solved a_first_geometry_problem.cpp

--- Semester3/Week11/a_first_geometry_problem.cpp
+++ Semester3/Week11/a_first_geometry_problem.cpp
@@ -10,11 +10,25 @@ int main() {
 
   // Write your code here
 
-  /*
-  int T; cin>>T;
-  while(T--){
+  int T;
+  cin >> T;
+  while (T--) {
+    char L, R, U, D;
+    cin >> L >> R >> U >> D;
 
+    int horizontal_axis = 1;
+    int vertical_axis = 1;
+
+    if (L - '0')
+      horizontal_axis += 10;
+    if (R - '0')
+      horizontal_axis += 10;
+    if (U - '0')
+      vertical_axis += 10;
+    if (D - '0')
+      vertical_axis += 10;
+
+    cout << horizontal_axis * vertical_axis << '\n';
   }
-  */
   return 0;
 }
