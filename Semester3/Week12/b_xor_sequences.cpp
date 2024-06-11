1a40c2a27279de55089d75ccc6a7ff08c42bae07
foy4748 <faisaljfcl@gmail.com>
2024-06-12 00:59:47 +0600

    Submited h_xor_sequences

--- Semester3/Week12/b_xor_sequences.cpp
+++ Semester3/Week12/b_xor_sequences.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 #define endl "\n"
-//#define ll long long int
+#define ll long long int
 
 using namespace std;
 
@@ -13,6 +13,22 @@ int main() {
   int T;
   cin >> T;
   while (T--) {
+    ll x, y;
+    cin >> x >> y;
+    ll count = 0;
+    for (int i = 0; i < 30; i++) {
+      bool flag1 = false;
+      bool flag2 = false;
+      if (x & (1LL << i))
+        flag1 = true;
+      if (y & (1LL << i))
+        flag2 = true;
+      if (flag1 == flag2)
+        count++;
+      else
+        break;
+    }
+    cout << (1LL << count) << '\n';
   }
   return 0;
 }
