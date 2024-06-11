4392351bfc9a8a4ed2c31833ad9b9c34a8eb5fa3
foy4748 <faisaljfcl@gmail.com>
2024-06-11 23:56:36 +0600

    Added b_xor_sequences.cpp

---
+++ Semester3/Week12/b_xor_sequences.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+//#define ll long long int
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  // Write your code here
+
+  int T;
+  cin >> T;
+  while (T--) {
+  }
+  return 0;
+}
