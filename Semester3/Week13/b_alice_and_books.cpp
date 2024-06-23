5ff9c4023ad84c73ae647c05a503eec20d12718e
foy4748 <faisaljfcl@gmail.com>
2024-06-24 02:00:20 +0600

    Added b_alice_and_books.cpp

--- Semester3/Week13/b_alice_and_books.cpp
+++ Semester3/Week13/b_alice_and_books.cpp
@@ -1,8 +1,18 @@
 #include <bits/stdc++.h>
 #define endl "\n"
-//#define ll long long int
+#define ll long long int
 
 using namespace std;
+
+bool comp(pair<int, int> a, pair<int, int> b) {
+  if (a.first > b.first) {
+    if (a.second > b.second)
+      return true;
+    else
+      return false;
+  } else
+    return false;
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
@@ -10,11 +20,20 @@ int main() {
 
   // Write your code here
 
-  /*
-  int T; cin>>T;
-  while(T--){
-
+  int T;
+  cin >> T;
+  while (T--) {
+    int N;
+    cin >> N;
+    ll max_sum = -1;
+    vector<int> v(N - 1);
+    for (int i = 0; i < N - 1; i++) {
+      cin >> v[i];
+    }
+    int lastElem;
+    cin >> lastElem;
+    sort(v.begin(), v.end());
+    cout << lastElem + v[N - 2] << '\n';
   }
-  */
   return 0;
 }
