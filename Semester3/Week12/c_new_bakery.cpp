10073a5c55a53822b1438b9ffd1c18041c3f8c4a
foy4748 <faisaljfcl@gmail.com>
2024-06-25 23:53:20 +0600

    Added d_deadly_laser

--- Semester3/Week12/c_new_bakery.cpp
+++ Semester3/Week12/c_new_bakery.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 #define endl "\n"
-//#define ll long long int
+#define ll long long int
 
 using namespace std;
 
@@ -13,6 +13,14 @@ int main() {
   int T;
   cin >> T;
   while (T--) {
+    ll N, a, b;
+    cin >> N >> a >> b;
+    if (b <= a) {
+      cout << N * a << '\n';
+    } else {
+      ll k = min(b - a + 1, N);
+      cout << (b - k + 1) * N + k * (k - 1) / 2 << '\n';
+    }
   }
   return 0;
 }
---
+++ Semester3/Week13/d_deadly_laser.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+#include <cstring>
+#define endl "\n"
+//#define ll long long int
+
+using namespace std;
+
+bool isFound = false;
+int max_r, max_c;
+int Ar, Ac, Br, Bc;
+const int Limit = 1005;
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+bool isVisited[Limit][Limit];
+int grid[Limit][Limit];
+
+bool isValid(int r, int c) {
+  if (r < 0 || c < 0 || r >= max_r || c >= max_c)
+    return false;
+  return true;
+}
+
+bool isEvaporated(int cr, int cc, int lsr, int lsc, int d) {
+  int X = abs(cr - lsr);
+  int Y = abs(cc - lsc);
+  int distance = X + Y;
+  return distance <= d;
+}
+
+void bfs(int lsr, int lsc, int d) {
+  queue<pair<int, int>> q;
+  int sr = 1;
+  int sc = 1;
+  isVisited[sr][sc] = true;
+  grid[0][0] = 0;
+  q.push({sr, sc});
+
+  while (!q.empty()) {
+    pair<int, int> p = q.front();
+    q.pop();
+    int pr = p.first;
+    int pc = p.second;
+
+    for (int i = 0; i < 4; i++) {
+      int nr = pr + dx[i];
+      int nc = pc + dy[i];
+
+      if (isValid(nr, nc) && !isVisited[nr][nc] &&
+          isEvaporated(nr, nc, lsr, lsc, d)) {
+        isVisited[nr][nc] = true;
+        grid[nr][nc] = grid[pr][pc] + 1;
+        q.push({nr, nc});
+      }
+    }
+  }
+}
+
+int main() {
+  // Write your code here
+  int T;
+  cin >> T;
+  while (T--) {
+    memset(isVisited, false, sizeof(isVisited));
+    cin >> max_r >> max_c;
+    int lsr, lsc, d;
+    cin >> lsr >> lsc;
+    cin >> d;
+
+    grid[max_r - 1][max_c - 1] = -1;
+    bfs(lsr, lsc, d);
+    cout << grid[max_r - 1][max_c - 1] << '\n';
+  }
+
+  return 0;
+}
