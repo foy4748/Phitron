f1f03c8c6ac83d4a0343ae9beea9b701fb2841ca
foy4748 <faisaljfcl@gmail.com>
2024-07-01 23:53:36 +0600

    Added Trick or Treat

---
+++ Semester3/Week14/trick_or_treat.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long int
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  // Write your code here
+
+  int T;
+  cin >> T;
+  while (T--) {
+    int N, M;
+    cin >> N >> M;
+    map<int, int> mp;
+    vector<int> arrA(N);
+    vector<int> arrB(N);
+    ll count = 0;
+    for (int i = 0; i < N; i++) {
+      cin >> arrA[i];
+      mp[arrA[i] % M]++;
+    }
+    for (int i = 0; i < N; i++) {
+      cin >> arrB[i];
+      int y = arrB[i] % M;
+      int x = (M - y) % M;
+
+      count += mp[x];
+    }
+
+    cout << count << '\n';
+  }
+  return 0;
+}
