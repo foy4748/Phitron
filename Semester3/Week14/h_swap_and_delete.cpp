816f21867a87470db71d3959f307d0900109c345
foy4748 <faisaljfcl@gmail.com>
2024-07-07 23:51:41 +0600

    Addeded Week15

--- Semester3/Week14/h_swap_and_delete.cpp
+++ Semester3/Week14/h_swap_and_delete.cpp
@@ -13,14 +13,31 @@ int main() {
   int T;
   cin >> T;
   while (T--) {
-    int N;
-    cin >> N;
-    ll count[2] = {0, 0};
-    for (int i = 0; i < N; i++) {
-      char x;
-      cin >> x;
+    string v;
+    cin >> v;
+    int count[2] = {0, 0};
+    for (int i = 0; i < v.size(); i++) {
+      char x = v[i];
       int num = x - '0';
       count[num]++;
+    }
+
+    if (count[0] == v.size() || count[1] == v.size())
+      cout << v.size() << '\n';
+    else if (count[0] == count[1])
+      cout << 0 << '\n';
+    else {
+      int i;
+      for (i = 1; i <= v.size(); i++) {
+        int opposite = v[i - 1] == '0' ? 1 : 0;
+        if (count[opposite] >= 1) {
+          count[opposite]--;
+        } else {
+          break;
+        }
+      }
+
+      cout << v.size() - i + 1 << '\n';
     }
   }
   return 0;
