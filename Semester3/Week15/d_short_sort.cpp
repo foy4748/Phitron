46eca9b9a3376627c1e90a2cb24167c7c62de9b3
foy4748 <faisaljfcl@gmail.com>
2024-07-08 00:12:51 +0600

    Solved d_short_sort.cpp

--- Semester3/Week15/d_short_sort.cpp
+++ Semester3/Week15/d_short_sort.cpp
@@ -10,11 +10,18 @@ int main() {
 
   // Write your code here
 
-  /*
-  int T; cin>>T;
-  while(T--){
-
+  int T;
+  cin >> T;
+  while (T--) {
+    string goal = "abc";
+    int matched = -1;
+    for (int i = 0; i < 3; i++) {
+      char x;
+      cin >> x;
+      if (goal[i] == x)
+        matched = i;
+    }
+    matched == -1 ? cout << "NO" << '\n' : cout << "YES" << '\n';
   }
-  */
   return 0;
 }
