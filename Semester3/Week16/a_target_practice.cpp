96bd67b2445596a13ad4f30a13f90ac67f73bcb7
foy4748 <faisaljfcl@gmail.com>
2024-07-15 00:21:23 +0600

    Solved a_target_practice

--- Semester3/Week16/a_target_practice.cpp
+++ Semester3/Week16/a_target_practice.cpp
@@ -4,27 +4,42 @@
 
 using namespace std;
 
-int score(int x, int y, int size) {
+int calculate_score(int x, int y, int size) {
+  //  0 based indexing
+  size = size - 1;
   int score = 1;
   for (int i = 0; i < 10; i++) {
-    bool logic = x == i || y == i || x == (size - 1) - i || y == (size - 1);
-    if (logic)
-      return 1;
-    else
-      return 0;
+    bool row = x == i || x == size - i;
+    bool col = y == i || y == size - i;
+    if (row || col) {
+      score += (i % 5);
+      break;
+    }
   }
+  return score;
 }
 
 void solve() {
   // Write your code here
+  int score = 0;
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
+      char x;
+      cin >> x;
+      if (x == 'X')
+        score += calculate_score(i, j, 10);
+    }
+  }
+  cout << score << '\n';
 }
 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
-  /* int T; cin>>T; */
-  /* while(T--) */
-  solve();
+  int T;
+  cin >> T;
+  while (T--)
+    solve();
   return 0;
 }
