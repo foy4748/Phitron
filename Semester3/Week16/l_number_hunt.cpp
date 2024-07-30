d50068bbc335706b0780777aeff404e3b3f3b005
foy4748 <faisaljfcl@gmail.com>
2024-07-30 23:56:47 +0600

    Added number hunt

---
+++ Semester3/Week16/l_number_hunt.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long int
+
+using namespace std;
+
+void solve() {
+  // Write your code here
+  int X;
+  cin >> X;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int T;
+  cin >> T;
+  while (T--)
+    solve();
+  return 0;
+}
