dfbb959894ee28e06de07c2ae0840af6131770d7
foy4748 <faisaljfcl@gmail.com>
2024-08-10 23:59:18 +0600

    Added xorry

---
+++ Semester3/Week16/xorry.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long int
+
+using namespace std;
+
+void solve() {
+  // Write your code here
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int T;
+  cin >> T;
+  while (T--)
+    solve();
+  return 0;
+}
